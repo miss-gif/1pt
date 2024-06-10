838ade098a24b34bcd900231564ae5f58fdaffa4
miss-gif <svx327@naver.com>
2024-06-10 21:12:00 +0900

    Feat : Custom hook 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
 import "./App.css";
-
-import { useRef, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { dummy } from "./api/dummy";
+import { AuthProvider } from "./pages/AuthContext";
+
 import CalendarPage from "./pages/CalendarPage";
 import LoginPage from "./pages/LoginPage";
-import Header from "./pages/header";
 import SignupPage from "./pages/SignupPage";
-import { AuthProvider } from "./pages/AuthContext";
+import Header from "./pages/header";
 
 import Home from "./pages/Home";
 import Notice from "./pages/notice/Notice";
 import PostWrite from "./pages/notice/PostWrite";
-import PostDetail from "./pages/notice/PostDetail"; // 게시글 상세 페이지
+import PostDetail from "./pages/notice/PostDetail";
 import PostEdit from "./pages/notice/PostEdit";
 
+import usePosts from "./hooks/usePosts"; // Custom hook import
+
 function App() {
-  const [posts, setPosts] = useState(dummy); // 게시글 데이터 상태
-  const postIdRef = useRef(26); // useRef를 App 컴포넌트에서 관리
-
-  const addPost = (newPost) => {
-    setPosts([newPost, ...posts]); // 새로운 게시글 추가
-    postIdRef.current += 1; // postId 업데이트
-  };
-
-  const handleDelete = (postId) => {
-    const updatedPosts = posts.filter((post) => post.postId !== postId);
-    setPosts(updatedPosts);
-  };
-
-  const handleUpdate = (updatedPost) => {
-    const updatedPosts = posts.map((post) =>
-      post.postId === updatedPost.postId ? updatedPost : post
-    );
-    setPosts(updatedPosts); // 수정된 게시글 목록으로 상태 업데이트
-  };
+  const { posts, addPost, handleDelete, handleUpdate, postIdRef } = usePosts();
 
   return (
     <AuthProvider>
@@ -55,7 +37,6 @@ function App() {
             path="/notice/edit/:postId"
             element={<PostEdit posts={posts} onUpdate={handleUpdate} />}
           />
-          {/* 추가된 라우터 */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
